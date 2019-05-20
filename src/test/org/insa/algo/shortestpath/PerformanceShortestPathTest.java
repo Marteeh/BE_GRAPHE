0d2131717e1d485ff00044d0f5166b4b61c4b099
Marteeh <33930283+Marteeh@users.noreply.github.com>
2019-05-20 12:03:17 +0200

    Update PerformanceShortestPathTest.java

--- src/test/org/insa/algo/shortestpath/PerformanceShortestPathTest.java
+++ src/test/org/insa/algo/shortestpath/PerformanceShortestPathTest.java
@@ -36,55 +36,36 @@ public class PerformanceShortestPathTest {
     @BeforeClass
     public static void initAll() throws Exception {
 
-        nombreTest=10;
-        borneInf=0;
-        borneSup=5;
+        nombreTest = 10;
+        borneInf = 0;
+        borneSup = 5;
 
         separateur = ";";
         // Init la carte
-        String mapName="/home/kompe/Bureau/Doc_BE/Maps/mauritius-island.mapgr";
+        String mapName="chemin map";
 
         GraphReader reader=new BinaryGraphReader(new DataInputStream(new BufferedInputStream(new FileInputStream(mapName))));
         carte=reader.read();
 
         rand = new Random();
-
-        // On random
-        writer = new PrintWriter("/home/kompe/Bureau/Projet/BE_Graphe/out/" + carte.getMapName() + borneInf + "-" + borneSup + ".csv", "UTF-8");
-        writer.println( "algo" + separateur + "originId" + separateur + "destId" + separateur + "distance_vol(m)" 
+        writer = new PrintWriter("/home/toutant/Bureau/" + carte.getMapName() + borneInf + "-" + borneSup + ".csv", "UTF-8");
+        writer.println("algo" + separateur + "originId" + separateur + "destId" + separateur + "distance_vol(m)" 
                               + separateur + "distance_Algo(m)" + separateur + "nombre_noeud" 
                               + separateur + "temps(ms)" + separateur + "nombreSommetVisite" );
     }
 
-    @Test
-    public void generateTest(){
-
-        int i=0;
-
-        while ( i < nombreTest ){
-            System.out.println( "Chemin trouvé : " + (i+1) );
-            System.out.println( "Avancement :" + (((double)i )/(((double) nombreTest)*100.0))+"%");
-            origine = carte.get(rand.nextInt(carte.size()-1));
-            destination = carte.get(rand.nextInt(carte.size()-1));
-            if (calculDijkstra(origine,destination)){
-                calculAStar(origine,destination);
-                i+=1;
-            }
-        }
-        writer.close();
-    }
-
+    //calcul dijkstra possible?
     public boolean calculDijkstra(Node origine, Node dest){
 
         boolean retour = false;
-        StringBuilder ret = new StringBuilder();
+        StringBuilder ecriture = new StringBuilder();
         double distance_vol = Point.distance(origine.getPoint(),dest.getPoint());
 
         //pour mesure le temps d'execution
-        long debut = System.nanoTime();
-        long end = System.nanoTime();
+        long debut;
+        long end;
 
-        ret.append("dijkstra" + separateur + origine.getId()+ separateur + dest.getId() + separateur + distance_vol + separateur);
+        ecriture.append("Dijkstra" + separateur + origine.getId() + separateur + dest.getId() + separateur + distance_vol + separateur);
 
         ShortestPathData data = new ShortestPathData(carte,origine,dest,ArcInspectorFactory.getAllFilters().get(0));
         ShortestPathAlgorithm dijkstraAlgo = new DijkstraAlgorithm(data);
@@ -100,32 +81,69 @@ public class PerformanceShortestPathTest {
             System.out.println(time);
             int size = solution.getPath().size()+1;
 
-            ret.append( solution.getPath().getLength() + separateur + size + separateur + time + separateur + solution.getNombreSommetVisite());
-            writer.println(ret.toString().replaceAll("\\.",","));
-            System.out.println(ret.toString().replaceAll("\\.",","));
+            ecriture.append( solution.getPath().getLength() + separateur + size + separateur + time + separateur + solution.getNombreSommetVisite());
+            writer.println(ecriture.toString().replaceAll("\\.",","));
+            System.out.println(ecriture.toString().replaceAll("\\.",","));
 
             retour=true;
         }
         return retour;
     }
 
+    //calcul Astar possible ?
     public boolean calculAStar(Node origine,Node dest){
-        Boolean retour=false;
-        StringBuilder ret=new StringBuilder();
-        double distance_vol= Point.distance(origine.getPoint(),dest.getPoint());
-        ret.append("astar"+separateur+origine.getId()+separateur+dest.getId()+separateur+distance_vol+separateur);
-        ShortestPathData data=new ShortestPathData(carte,origine,dest,ArcInspectorFactory.getAllFilters().get(0));
-        ShortestPathAlgorithm aStarAlgo=new AStarAlgorithm(data);
-        long debut=System.nanoTime();
-        ShortestPathSolution solution=aStarAlgo.doRun();
-        long end=System.nanoTime();
+
+        Boolean retour = false;
+        StringBuilder ecriture = new StringBuilder();
+        double distance_vol = Point.distance(origine.getPoint(),dest.getPoint());
+
+        //mesure temps
+        long debut;
+        long end;
+
+        ecriture.append("AStar" + separateur + origine.getId() + separateur + dest.getId() + separateur + distance_vol + separateur);
+
+        ShortestPathData data = new ShortestPathData(carte,origine,dest,ArcInspectorFactory.getAllFilters().get(0));
+        ShortestPathAlgorithm AStarAlgo = new AStarAlgorithm(data);
+
+        debut = System.nanoTime();
+        ShortestPathSolution solution=AStarAlgo.doRun();
+        end = System.nanoTime();
+
         double time=((double)(end-debut)/1000000.0);
-        if ((solution.isFeasible())&&(solution.getPath().getLength()<(float) borneSup*1000.0) && (solution.getPath().getLength()> (float) borneInf*1000.0)){
-            ret.append(solution.getPath().getLength()+separateur+solution.getPath().getArcs().size()+1+separateur+time+separateur+solution.getNombreSommetVisite());
-            writer.println(ret.toString().replaceAll("\\.",","));
-            System.out.println(ret.toString().replaceAll("\\.",","));
+
+        if ((solution.isFeasible())&&(solution.getPath().getLength()<(float) borneSup*1000.0) && (solution.getPath().getLength() > (float) borneInf*1000.0)){
+
+            System.out.println(time);
+            int size = solution.getPath().size()+1;
+
+            ecriture.append(solution.getPath().getLength() + separateur + size + separateur + time + separateur + solution.getNombreSommetVisite());
+            writer.println(ecriture.toString().replaceAll("\\.",","));
+            System.out.println(ecriture.toString().replaceAll("\\.",","));
+
             retour=true;
         }
         return retour;
+    }
+
+    @Test
+    public void generateTest(){
+
+        int i=0;
+
+        while ( i < nombreTest ){
+            System.out.println( "Chemin trouvé : " + (i+1) );
+            System.out.println( "Avancement :" + (((double)i )/(((double) nombreTest)*100.0))+"%");
+
+            //origine et destination pris au hasard sur la carte
+            origine = carte.get(rand.nextInt(carte.size()-1));
+            destination = carte.get(rand.nextInt(carte.size()-1));
+
+            if (calculDijkstra(origine,destination)){
+                calculAStar(origine,destination);
+                i+=1;
+            }
+        }
+        writer.close();
     }
 }
